b507c7ccb1b537774ab19e7d6fbb3db90e9ea8b5
Leo <lcskode@gmail.com>
2018-12-12 15:06:30 +0800

    Added App.js file and moved App component code from index.js file.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+// App Component
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
